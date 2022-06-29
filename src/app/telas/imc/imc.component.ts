e6fe868daac73c3fe4b8b3175b4831f4f7c3e90d
bj307 <62263060+bj307@users.noreply.github.com>
2022-06-29 00:35:25 -0300

    att

--- src/app/telas/imc/imc.component.ts
+++ src/app/telas/imc/imc.component.ts
@@ -14,18 +14,4 @@ export class ImcComponent implements OnInit {
     
   }
 
-  altura = document.querySelector('#altura')
-  alturaN: number = +altura;
-  altura3: number = (this.alturaN * this.alturaN)
-  peso = document.querySelector('#peso')
-  pesoN: number = +peso;
-  zero = 0
-  imcC = (this.pesoN * this.altura3)
-
-  calcIMC() {
-
-    const alt = this.altura
-    const imc = this.imcC
-    console.log(alt);
-  }
 }
